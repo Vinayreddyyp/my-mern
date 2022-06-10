72eb10633327b657b4cead27d9ffaaf1e503088e
vinayreddyyp <ypvinayreddy@gmail.com>
2022-06-10 11:17:54 -0700

    add user items for the list

---
+++ src/user/components/UserItem.js
@@ -0,0 +1,23 @@
+import "./style/UserItem.css";
+
+import Avatar from "../../shared/components/UIElements/Avatar";
+import React from "react";
+
+const UserItem = (props) => {
+	return (
+		<li className="user-item">
+			<div className="user-item__content">
+				<div className="user-item__image">
+					<Avatar src={props.image} alt={props.name} />
+				</div>
+				<div className="user-item__info">
+					<h2>{props.name}</h2>
+					<h3>
+						{props.placeCount} {props.placeCount === 1 ? "Place" : "Places"}
+					</h3>
+				</div>
+			</div>
+		</li>
+	);
+};
+export default UserItem;
