b6e963f9e5ecaaa0642201efe4567cca9eebd65b
vinayreddyyp <ypvinayreddy@gmail.com>
2022-06-16 11:59:49 -0700

    add User.js

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react";
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-    return (
-        <div>
-        <h1>this is users page </h1>
-        </div>
-    )
-}
+	const USERS = [
+		{
+			id: "u1",
+			name: "vinay",
+			image: "https://static.toiimg.com/photo/91440550/91440550.jpg?v=3",
+			placeCount: 3,
+		},
+	];
+	return <UsersList items={USERS} />;
+};
 
 export default Users;
