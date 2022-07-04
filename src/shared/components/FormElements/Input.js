5b88d2ea0d0aad0b40a8359463374f84ba43e147
vinayreddyyp <ypvinayreddy@gmail.com>
2022-07-03 19:06:52 -0700

    add input to the app

--- src/shared/components/FormElements/Input.js
+++ src/shared/components/FormElements/Input.js
@@ -1,18 +1,82 @@
 import "./Input.css";
 
-import React from "react";
+import React, { useEffect, useReducer } from "react";
+
+import { validate } from "../../util/validators";
+
+const inputReducer = (state, action) => {
+	switch (action.type) {
+		case "CHANGE":
+			return {
+				...state,
+				value: action.val,
+				isValid: validate(action.val, action.validators),
+			};
+		case "TOUCHED":
+			return {
+				...state,
+				isTouched: true,
+			};
+		default:
+			return state;
+	}
+};
 
 const Input = (props) => {
+	console.log("🚀 ~ file: Input.js ~ line 23 ~ Input ~ props", props);
+	const [inputState, dispatch] = useReducer(inputReducer, {
+		value: "",
+		isTouched: false,
+		isValid: false,
+	});
+	const { id, onInput } = props;
+	const { value, isValid } = inputState;
+
+	useEffect(() => {
+		onInput(id, value, isValid);
+	}, [id, value, isValid, onInput]);
+
+	const changeHandler = (event) => {
+		dispatch({
+			type: "CHANGE",
+			val: event.target.value,
+			validators: props.validators,
+		});
+	};
+	const touchHandler = () => {
+		dispatch({
+			type: "TOUCHED",
+		});
+	};
+
 	const element =
 		props.type === "input" ? (
-			<input type={props.type} placeholder={props.placeholder} id={props.id} />
+			<input
+				type={props.type}
+				value={inputState.value}
+				placeholder={props.placeholder}
+				onChange={changeHandler}
+				onBlur={touchHandler}
+				id={props.id}
+			/>
 		) : (
-			<textarea type={props.type} rows={props.rows || 3} />
+			<textarea
+				type={props.type}
+				onChange={changeHandler}
+				value={inputState.value}
+				onBlur={touchHandler}
+				rows={props.rows || 3}
+			/>
 		);
 	return (
-		<div className={`form-control`}>
+		<div
+			className={`form-control ${
+				!inputState.isValid && inputState.isTouched && "form-control--invalid"
+			}`}
+		>
 			<label htmlFor={props.id}>{props.label}</label>
 			{element}
+			{!inputState.isValid && inputState.isTouched && <p>{props.errorText}</p>}
 		</div>
 	);
 };
