b4c5cb979e63b8969e0c8b4dfda7672c24932085
vinayreddyyp <ypvinayreddy@gmail.com>
2022-07-05 14:23:01 -0700

    add user list to the app

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -1,24 +1,33 @@
-import './style/UsersList.css';
+import "./UsersList.css";
 
-import React from 'react';
-import UserItem from './UserItem';
+import Card from "../../shared/components/UIElements/Card";
+import React from "react";
+import UserItem from "./UserItem";
 
-const UserList = (props) => {
-    if(props.items.length === 0) {
-        return (
-            <div className="center">
-            <h2>No Users FOund</h2>
-            </div>
-        )
-    }
+const UsersList = (props) => {
+	if (props.items.length === 0) {
+		return (
+			<div className="center">
+				<Card>
+					<h2>No users found.</h2>
+				</Card>
+			</div>
+		);
+	}
 
-    return (
-        <ul>
-          {props.items.map(user => (
-              <UserItem key={user.id} id={user.id} image={user.image} name={user.name} placeCount={user.placeCount}/>
-          ))}
-        </ul>
-    )
-}
+	return (
+		<ul className="users-list">
+			{props.items.map((user) => (
+				<UserItem
+					key={user.id}
+					id={user.id}
+					image={user.image}
+					name={user.name}
+					placeCount={user.places}
+				/>
+			))}
+		</ul>
+	);
+};
 
-export default UserList;
+export default UsersList;
