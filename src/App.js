a11f4e415651b8e6dbbeecbe4f0bbc728fcc8ceb
vinayreddyyp <ypvinayreddy@gmail.com>
2022-07-07 12:33:04 -0700

    add new route to the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-import './App.css';
+import {
+	Redirect,
+	Route,
+	BrowserRouter as Router,
+	Switch,
+	useParams,
+} from "react-router-dom";
 
-import { BrowserRouter as Router } from 'react-router-dom';
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import NewPlace from "./places/pages/NewPlace";
+import React from "react";
+import UpdatePlace from "./places/pages/UpdatePlace";
+import UserPlaces from "./places/pages/UserPlaces";
+import Users from "./user/pages/Users";
 
 const App = () => {
-    return <Router>
-        
-         </Router>;
-}
+	debugger;
+	return (
+		<Router>
+			<MainNavigation />
+			<main>
+				<Switch>
+					<Route path="/" exact>
+						<Users />
+					</Route>
+					<Route path="/:userId/places" exact>
+						<UserPlaces />
+					</Route>
+					<Route path="/places/new" exact>
+						<NewPlace />
+					</Route>
+					<Route path="/places/:placeId">
+						<UpdatePlace />
+					</Route>
+					<Redirect to="/" />
+				</Switch>
+			</main>
+		</Router>
+	);
+};
 
 export default App;
